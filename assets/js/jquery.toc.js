76561a0a7631d5308811ef6a4675af67582beb73
xoyabc <1031138448@qq.com>
2018-05-23 00:25:41 +0800

    Update jquery.toc.js

--- assets/js/jquery.toc.js
+++ assets/js/jquery.toc.js
@@ -1,17 +1,15 @@
 // https://github.com/ghiculescu/jekyll-table-of-contents
+// Updated by http://mazhuang.org
 (function($){
   $.fn.toc = function(options) {
     var defaults = {
       noBackToTopLinks: false,
-      title: '<i>Jump to...</i>',
-      minimumHeaders: 3,
+      title: '文章目录',
+      minimumHeaders: 2,
       headers: 'h1, h2, h3, h4, h5, h6',
-      listType: 'ol', // values: [ol|ul]
+      listType: 'ul', // values: [ol|ul]
       showEffect: 'show', // values: [show|slideDown|fadeIn|none]
-      showSpeed: 'slow', // set to 0 to deactivate effect
-      classes: { list: '',
-                 item: ''
-               }
+      showSpeed: 0 // set to 0 to deactivate effect
     },
     settings = $.extend(defaults, options);
 
@@ -19,11 +17,6 @@
       return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
         return '%' + c.charCodeAt(0).toString(16);
       });
-    }
-
-    function createLink (header) {
-      var innerText = (header.textContent === undefined) ? header.innerText : header.textContent;
-      return "<a href='#" + fixedEncodeURIComponent(header.id) + "'>" + innerText + "</a>";
     }
 
     var headers = $(settings.headers).filter(function() {
@@ -36,6 +29,7 @@
     }), output = $(this);
     if (!headers.length || headers.length < settings.minimumHeaders || !output.length) {
       $(this).hide();
+      $('.post-directory-title').css('display', 'none');
       return;
     }
 
@@ -50,13 +44,14 @@
       none: function() { output.html(html); }
     };
 
-    var get_level = function(ele) { return parseInt(ele.nodeName.replace("H", ""), 10); };
+    var get_level = function(ele) { return parseInt(ele.nodeName.replace("H", ""), 10); }
     var highest_level = headers.map(function(_, ele) { return get_level(ele); }).get().sort()[0];
     var return_to_top = '<i class="icon-arrow-up back-to-top"> </i>';
 
     var level = get_level(headers[0]),
-      this_level,
-      html = settings.title + " <" +settings.listType + " class=\"" + settings.classes.list +"\">";
+    this_level,
+    html = "<p><strong class=\"toc-title\">" + settings.title + "</strong></p>\n";
+    html += " <"+settings.listType+" class=\"toc\">";
     headers.on('click', function() {
       if (!settings.noBackToTopLinks) {
         window.location.hash = this.id;
@@ -68,20 +63,27 @@
       if (!settings.noBackToTopLinks && this_level === highest_level) {
         $(header).addClass('top-level-header').after(return_to_top);
       }
-      if (this_level === level) // same level as before; same indenting
-        html += "<li class=\"" + settings.classes.item + "\">" + createLink(header);
-      else if (this_level <= level){ // higher level than before; end parent ol
-        for(var i = this_level; i < level; i++) {
+      if (this_level === level) {// same level as before; same indenting
+        html += "<li class=\"toc-item toc-level-" + this_level + "\">";
+        html += "<a class=\"jumper\" href='#" + fixedEncodeURIComponent(header.id) + "'>";
+        html += "<span class='toc-text'>" + header.innerHTML + "</span>";
+        html += "</a>";
+
+      } else if (this_level <= level){ // higher level than before; end parent ol
+        for(i = this_level; i < level; i++) {
           html += "</li></"+settings.listType+">"
         }
-        html += "<li class=\"" + settings.classes.item + "\">" + createLink(header);
+        html += "<li class='toc-item toc-level-" + this_level + "'><a class=\"jumper\" href='#" + fixedEncodeURIComponent(header.id) + "'>";
+        html += "<span class='toc-text'>" + header.innerHTML + "</span>";
+        html += "</a>";
       }
       else if (this_level > level) { // lower level than before; expand the previous to contain a ol
         for(i = this_level; i > level; i--) {
-          html += "<" + settings.listType + " class=\"" + settings.classes.list +"\">" +
-                  "<li class=\"" + settings.classes.item + "\">"
+          html += "<"+settings.listType+" class='toc-child'><li class='toc-item toc-level-" + i + "'>"
         }
-        html += createLink(header);
+        html += "<a class=\"jumper\" href='#" + fixedEncodeURIComponent(header.id) + "'>";
+        html += "<span class='toc-text'>" + header.innerHTML + "</span>";
+        html += "</a>";
       }
       level = this_level; // update for the next one
     });
@@ -96,3 +98,84 @@
     render[settings.showEffect]();
   };
 })(jQuery);
+
+$(document).ready(function(){
+  $('.post-directory').toc();
+
+  var fixmeTop = $('#post-directory-module').offset().top;
+  var tocSections = $('.clickable-header');
+  var tocSectionOffsets = [];
+
+  var calculateTocSections = function(){
+    tocSectionOffsets = [];
+    tocSections.each(function(index, section){
+      tocSectionOffsets.push(section.offsetTop);
+    })
+  }
+  calculateTocSections();
+  // Calculates the toc section offsets, which can change as images get loaded
+  $(window).bind('load', calculateTocSections);
+
+  var highlightTocSection = function(){
+    var highlightIndex = 0;
+    var sectionsCount = tocSectionOffsets.length;
+    var currentScroll = $(window).scrollTop();
+
+    if (currentScroll+60 > tocSectionOffsets[sectionsCount-1]) {
+      highlightIndex = sectionsCount;
+    } else {
+      for (var i=0; i<sectionsCount; i++) {
+        if (currentScroll+60 <= tocSectionOffsets[i]) {
+          highlightIndex = i;
+          break;
+        }
+      }
+    }
+    if (highlightIndex == 0) {
+      highlightIndex += 1;
+    }
+    $('.toc-item .jumper').removeClass('on');
+    $('.toc-item .jumper').eq(highlightIndex-1).addClass('on');
+  }
+  highlightTocSection();
+
+  var updateTocHeight = function() {
+      var height = document.documentElement.clientHeight;
+      height = height || 'auto';
+      $('.post-directory').css('max-height', height);
+  }
+
+  $(window).scroll(function() {
+    var currentScroll = $(window).scrollTop();
+    if (currentScroll >= fixmeTop) {
+      $('#post-directory-module').css({
+        top: '0',
+        position: 'fixed',
+        width: 'inherit'
+      });
+      $('.post-directory').css('overflow', 'auto');
+    } else {
+      $('#post-directory-module').css({
+        position: 'inherit',
+        width: 'inherit'
+      });
+      $('.post-directory').css('overflow', 'hidden');
+      $('.post-directory').scrollTop(0);
+    }
+
+    highlightTocSection();
+  });
+
+  updateTocHeight();
+
+  $(window).on("resize", function() {
+      updateTocHeight();
+  });
+});
+
+$(".jumper").on("click", function( e ) {
+  e.preventDefault();
+  $("body, html").animate({
+    scrollTop: $( $(this).attr('href') ).offset().top
+  }, 600);
+});
